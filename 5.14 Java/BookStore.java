591c4957ef6ed2539034f558aab4ca954104713b
dburtonthompson <62103558+dburtonthompson@users.noreply.github.com>
2020-05-14 16:54:43 -0400

    lab

---
+++ 5.14 Java/BookStore.java
@@ -0,0 +1,43 @@
+import java.nio;
+import java.text;
+import java.net;
+import java.nio.files;
+import java.math;
+import java.time;
+import java.scanner;
+
+/**
+ * BookStore
+ */
+public class BookStore {
+
+    static void store() {
+        System.out.println("This is my bookstore, De Books!");
+    }
+
+    static void location() {
+        System.out.println("My store is located at 2989 Sycamore St.");
+    }
+
+    static void hours() {
+        System.out.println("De Books is open Tuesday - Saturday 10 am to 6 pm.");
+    }
+
+    static void size() {
+        System.out.println("The square foot of of our location is 1000sf.");
+    }
+
+    static void books() {
+        System.out.println("De Books has all types of books new and used.");
+    }
+
+    public static void main(String[] args) {
+
+        store();
+        location();
+        hours();
+        size();
+        books();
+
+    }
+}
